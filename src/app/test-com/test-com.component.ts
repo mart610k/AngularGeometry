1f7d844b1da1f7a970f9560f620263915a65e5b8
mart610k <martin-baage@hotmail.com>
2020-08-10 13:55:10 +0200

    Square and calculation works now

--- src/app/test-com/test-com.component.ts
+++ src/app/test-com/test-com.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Triangle } from "../shapes/triangle";
+import { Square } from "../shapes/square";
+import { createLoweredSymbol } from '@angular/compiler';
 
 @Component({
   selector: 'app-test-com',
@@ -7,15 +9,41 @@ import { Triangle } from "../shapes/triangle";
   styleUrls: ['./test-com.component.css']
 })
 export class TestComComponent implements OnInit {
+  lenghID : number;
+  heightID : number;
+  calculatedArea : number = 0 ;
 
+  
   constructor() { 
-    let triable = new Triangle(3,5);
+    let triable = new Triangle(2,5);
     console.log(triable.calcuateArea());
   }
   
-  
+  doSomething() : void{
+    console.log("---------------------------------------------");
+    console.log(this.lenghID);
+    console.log(this.heightID);
+    let square = new Square(this.lenghID,this.heightID);
+    this.calculatedArea = square.calcuateArea();
+    console.log(square.calcuateArea());
+  }
+  //TODO: Find out how to actually set up an event to save it for real.
+  //(its currently only through a switch case)... 
+  change(event) {
+    // console.log(event.target.value);
+    // console.log(event.target.id);
+    switch(event.target.id){
+      case "HeightID":
+        this.heightID = event.target.value;
+        break;
+     case"LengthID":
+        this.lenghID = event.target.value
+        break;
+    }
+
+  }
   
   ngOnInit(): void {
   }
-
+  
 }
